282fa2f39088a0bd9c2f8bd1e32ce145bf5612cb
ZaddyAI <gambhir.poudel@gmail.com>
2025-06-19 21:19:06 +0545

    Learn to use useState() & useEffect()

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 function Counter() {
     // useState is a hook that allows you to add state to functional components
     // const [state, setState] = useState(initialState);
@@ -6,21 +7,33 @@ function Counter() {
     // here setCounter is a function that updates the counter state
     // counter is the current state value
     const [counter, setCounter] = useState(0);
+
     // let counter = 0;
-    const add = () => {
-        //  console.log("Counter: ", counter);
-        setCounter(counter + 1)
-    }
+    // const add = () => {
+    //     //  console.log("Counter: ", counter);
+    //     setCounter(counter + 1)
+    // }
     const sub = () => {
         //  console.log("Counter: ", counter);
         setCounter(counter - 1)
     }
+
+    // using useEffect
+    // useEffect is a hook that allows you to perform side effects in functional components
+    // it takes a function as the first argument and an array of dependencies as the second argument
+    // the function will be called after the component renders and whenever the dependencies change
+    // in this case, we are logging the counter value to the console whenever it changes
+    useEffect(() => {
+        console.log(`Count is now: ${counter}`)
+    }, [counter]);
+
     return (
         <>
-            <h1>Counter: {counter}</h1>
-            <div className="buttons">
-                <button onClick={add}>Increment</button>
-                <button onClick={sub}>Decrement</button>
+            <div className="bg-red-400 text-white p-2 rounded-xl text-center mb-4">Counter: {counter}</div>
+            <div className="flex justify-center gap-4 mb-4" >
+                {/* <button onClick={add} className='bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Increment</button> */}
+                <button onClick={() => setCounter(counter + 1)} className='bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>Increment</button>
+                <button onClick={sub} className='bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded'>Decrement</button>
             </div>
         </>
     );
